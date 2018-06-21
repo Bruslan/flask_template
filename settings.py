7f497f03cb4ed35aed74cb62ba7ab32719859a94
BulBul <bulbul@BulBul.fritz.box>
2018-06-21 20:56:59 +0200

    req

--- settings.py
+++ settings.py
@@ -5,10 +5,10 @@ global db
 global app
 app = Flask(__name__)
 db = MongoEngine()
-app.config['MONGODB_DB'] = 'TODO'
-app.config['MONGODB_HOST'] = 'localhost'
-app.config['MONGODB_PORT'] = 27017
-app.config['MONGODB_USERNAME'] = ''
-app.config['MONGODB_PASSWORD'] = ''
+app.config['MONGODB_DB'] = 'paarfit'
+app.config['MONGODB_HOST'] = 'ds263660.mlab.com'
+app.config['MONGODB_PORT'] = 63660
+app.config['MONGODB_USERNAME'] = 'bruslan'
+app.config['MONGODB_PASSWORD'] = 'test123'
 app.secret_key = "Am I being w@tched? Damn yes!"
 db.init_app(app)
